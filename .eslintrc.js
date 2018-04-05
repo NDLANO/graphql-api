be403ad74567857e805a30d6bffc8be6992ff1e3
Øyvind Marthinsen <oma@knowit.no>
2018-04-05 09:23:30 +0200

    feat: add typescript parser for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  parser: 'typescript-eslint-parser',
   env: {
     node: true,
     es6: true,
@@ -9,5 +10,13 @@ module.exports = {
   extends: 'eslint:recommended',
   rules: {
     'no-console': 0,
+    'no-shadow': 'error',
+    'no-shadow-restricted-names': 'error',
+    eqeqeq: 'error',
+    'no-else-return': 'error',
+    'no-eval': 'error',
+    'no-extra-bind': 'error',
+    'no-extra-label': 'error',
+    'no-with': 'error',
   },
 };
