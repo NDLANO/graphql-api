eaa18fcf814cdca9c6e70c3ce8e4b50640528336
Gunnar Velle <gunnar.velle@gmail.com>
2019-06-12 10:07:29 +0200

    Use pagination to get metadata

--- src/api/articleApi.ts
+++ src/api/articleApi.ts
@@ -8,6 +8,7 @@
 
 import { fetch, resolveJson } from '../utils/apiHelpers';
 import { localConverter } from '../config';
+import { Response } from 'apollo-env';
 
 export async function fetchArticle(
   params: {
@@ -28,21 +29,39 @@ export async function fetchArticle(
   return resolveJson(response);
 }
 
+async function fetchArticlesPage(
+  articleIds: string[],
+  context: Context,
+  pageSize: number,
+  page: number,
+) {
+  return fetch(
+    `/article-api/v2/articles/?ids=${articleIds.join(',')}&language=${
+      context.language
+    }&pageSize=${pageSize}&page=${page}&fallback=true`,
+    context,
+  ).then(res => res.json());
+}
+
 export async function fetchArticles(
   articleIds: string[],
   context: Context,
 ): Promise<GQLMeta[]> {
-  const response = await fetch(
-    `/article-api/v2/articles/?ids=${articleIds.join(',')}&language=${
-      context.language
-    }&fallback=true`,
-    context,
-  );
-  const json = await resolveJson(response);
+  const pageSize = 100;
+  const firstPage = await fetchArticlesPage(articleIds, context, pageSize, 1);
+  const numberOfPages = Math.ceil(firstPage.totalCount / firstPage.pageSize);
+
+  const requests = [firstPage];
+  for (let i = 1; i < numberOfPages; i += 1) {
+    requests.push(fetchArticlesPage(articleIds, context, pageSize, i));
+  }
+  const results = await Promise.all(requests);
+  const articles = results.reduce((acc, res) => [...acc, ...res.results], []);
+
   // The api does not always return the exact number of results as ids provided.
   // So always map over ids so that dataLoader gets the right amount of results in correct order.
   return articleIds.map(id => {
-    const article = json.results.find((item: { id: number }) => {
+    const article = articles.find((item: { id: number }) => {
       return item.id.toString() === id;
     });
     if (article) {
