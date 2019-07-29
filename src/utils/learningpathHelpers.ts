307bde6af6ef4d0cce73c724cacf2d6c4953b867
Christian Peter <christian150790@gmail.com>
2019-07-29 15:59:10 +0200

    Removes unused code

--- src/utils/learningpathHelpers.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2019-present, NDLA.
- * This source code is licensed under the GPLv3 license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-export function getArticleIdFromEmbedUrl(url: string): string {
-  return url.replace('urn:article:', '');
-}
