49ac8c8e0265131a00f1f2a64e23502d71e07509
Henrik Liodden <henrik.liodden@knowit.no>
2021-03-12 09:54:49 +0100

    Undo schema.d.ts changes

--- src/types/schema.d.ts
+++ src/types/schema.d.ts
@@ -2663,7 +2663,6 @@ declare global {
   export interface GQLFrontPageResourcesTypeResolver<TParent = any> {
     results?: FrontPageResourcesToResultsResolver<TParent>;
     totalCount?: FrontPageResourcesToTotalCountResolver<TParent>;
-    suggestions?: FrontPageResourcesToSuggestionsResolver<TParent>;
   }
   
   export interface FrontPageResourcesToResultsResolver<TParent = any, TResult = any> {
@@ -2672,10 +2671,6 @@ declare global {
   
   export interface FrontPageResourcesToTotalCountResolver<TParent = any, TResult = any> {
     (parent: TParent, args: {}, context: any, info: GraphQLResolveInfo): TResult | Promise<TResult>;
-  }
-
-  export interface FrontPageResourcesToSuggestionsResolver<TParent = any, TResult = any> {
-    (parent: TParent, args: {}, context: any, info: GraphQLResolveInfo): TResult | Promise<TResult>
   }
   
   export interface GQLFrontpageSearchResultTypeResolver<TParent = any> {
