b0ad5bb963d918cf8323125efd68241eccdce3f8
Gunnar Velle <gunnar.velle@gmail.com>
2022-03-04 14:47:53 +0100

    Forgot to add new files.

---
+++ src/api/uptimeApi.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2022-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { Octokit } from "@octokit/rest";
+import { uptimeOwner, uptimeRepo } from '../config';
+
+export async function fetchUptimeIssues(): Promise<GQLUptimeAlert[]> {
+    const octokit = new Octokit();
+    const { data } = await octokit.rest.issues.listForRepo({
+        owner: uptimeOwner,
+        repo: uptimeRepo,
+        state: 'all',
+        labels: 'maintenance'
+    });
+    return data;
+}
---
+++ src/resolvers/uptimeResolvers.ts
@@ -0,0 +1,13 @@
+import { fetchUptimeIssues } from "../api";
+
+export const Query = {
+    async alerts(
+      _: any,
+      __: any,
+      context: ContextWithLoaders,
+    ): Promise<GQLUptimeAlert[]> {
+      return fetchUptimeIssues();
+    },
+  };
+
+  export const resolvers = {};
