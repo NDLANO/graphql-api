388636ed5f86d02f1ccf6e66a9ffb841bc581495
Gunnar Velle <gunnar.velle@gmail.com>
2023-02-21 14:46:50 +0100

    forgot format :(

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ const app = express();
 
 // compress all responses
 app.use(compression());
-app.use(express.json({limit: '1mb'}));
+app.use(express.json({ limit: '1mb' }));
 
 function getAcceptLanguage(request: Request): string {
   const language = request.headers['accept-language'];
