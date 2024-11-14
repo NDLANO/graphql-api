65dae62cfefaae0ac33b6545700fe114af5a0700
Gunnar Velle <gunnar.velle@gmail.com>
2024-11-14 18:20:38 +0100

    Fix: add isActive to context. Is false for archived subjects.

--- src/types/schema.d.ts
+++ src/types/schema.d.ts
@@ -2228,6 +2228,7 @@ export type GQLTaxonomyContext = {
   __typename?: 'TaxonomyContext';
   breadcrumbs: Array<Scalars['String']>;
   contextId: Scalars['String'];
+  isActive: Scalars['Boolean'];
   name: Scalars['String'];
   parentIds: Array<Scalars['String']>;
   parents?: Maybe<Array<GQLTaxonomyCrumb>>;
@@ -4366,6 +4367,7 @@ export type GQLTaxBaseResolvers<ContextType = any, ParentType extends GQLResolve
 export type GQLTaxonomyContextResolvers<ContextType = any, ParentType extends GQLResolversParentTypes['TaxonomyContext'] = GQLResolversParentTypes['TaxonomyContext']> = {
   breadcrumbs?: Resolver<Array<GQLResolversTypes['String']>, ParentType, ContextType>;
   contextId?: Resolver<GQLResolversTypes['String'], ParentType, ContextType>;
+  isActive?: Resolver<GQLResolversTypes['Boolean'], ParentType, ContextType>;
   name?: Resolver<GQLResolversTypes['String'], ParentType, ContextType>;
   parentIds?: Resolver<Array<GQLResolversTypes['String']>, ParentType, ContextType>;
   parents?: Resolver<Maybe<Array<GQLResolversTypes['TaxonomyCrumb']>>, ParentType, ContextType>;
