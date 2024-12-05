b2006639898a0eb96ce44dcfabd4d51fa57dd28f
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-12-05 13:23:40 +0100

    feat: upgrade to eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2024-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+// @ts-check
+
+import config from "eslint-config-ndla";
+import tseslint from "typescript-eslint";
+import graphqlPlugin from "@graphql-eslint/eslint-plugin";
+
+export default tseslint.config(
+  ...config,
+  {
+    ignores: ["**/schema.d.ts"],
+  },
+  {
+    files: ["**/*.graphql"],
+    languageOptions: {
+      parser: graphqlPlugin.parser,
+      parserOptions: {
+        graphQLConfig: {
+          schema: "./src/schema.ts",
+          documents: "**/*.ts",
+        },
+      },
+    },
+    plugins: {
+      "@graphql-eslint": graphqlPlugin,
+    },
+    rules: {
+      ...graphqlPlugin.configs["flat/schema-recommended"].rules,
+      "@graphql-eslint/known-type-names": "error",
+      "@graphql-eslint/require-description": 0,
+      "@graphql-eslint/strict-id-in-types": 0,
+      "@graphql-eslint/no-typename-prefix": 0,
+      "@graphql-eslint/no-unreachable-types": 0,
+    },
+  },
+);
